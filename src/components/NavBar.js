47dac42d00d4fa05e7791d8536d1e7b6eb29aecc
yoelwildanto <yoel.wildantop@gmail.com>
2023-10-06 08:46:34 +0700

    update number count

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,14 +6,28 @@ import axios from "axios";
 function Navbar() {
   const [userCount, setUserCount] = useState(0);
 
+  // useEffect(() => {
+  //   axios.get("http://localhost:3000/users").then((response) => {
+  //     setUserCount(response.data.length);
+  //   });
+  // }, []);
+
   useEffect(() => {
+    updateCount();
+
+    const intervalId = setInterval(updateCount, 1000); 
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const updateCount = () => {
     axios.get("http://localhost:3000/users").then((response) => {
       setUserCount(response.data.length);
     });
-  }, []);
+  };
 
   return (
-    <nav>
+    <nav className="navbar">
       <div>Logo</div>
       <div className="user-count">Number of Users: {userCount}</div>
     </nav>
