6710c9effc130e775112be63532702920d005d1d
yoelwildanto <yoel.wildantop@gmail.com>
2023-10-09 01:20:28 +0700

    sidebar

---
+++ src/Sibar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import SidebarOption from "./SidebarOption";
+import {BsTwitter, BsBookmark} from "react-icons/bs"
+import {BiHash, BiSolidHomeCircle} from "react-icons/bi"
+import {CiMail} from "react-icons/ci"
+import {CgMoreO} from "react-icons/cg"
+import {GoBell} from "react-icons/go"
+// import {HiOutlineDotsCircleHorizontal} from "react-icons/hi"
+import {RiFileList2Line} from "react-icons/ri"
+import { Button } from "@chakra-ui/react";
+
+
+import "./Sidebar.css"
+
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+        <BsTwitter className="sidebar__twitterIcon"/>
+
+        <SidebarOption active Icon={BiSolidHomeCircle} text="Home"/>
+        <SidebarOption  Icon={BiHash} text="Explore"/>
+        <SidebarOption  Icon={GoBell} text="Notifications"/>
+        <SidebarOption  Icon={CiMail} text="Messages"/>
+        <SidebarOption  Icon={BsBookmark} text="Bookmarks"/>
+        <SidebarOption  Icon={RiFileList2Line} text="Lists"/>
+        {/* <SidebarOption  Icon={""} text="Profile"/> */}
+        <SidebarOption  Icon={CgMoreO} text="More"/>
+      
+        <Button mt={"70px"} w={"100%"} color={"white"} bg={"twitter.400"}>Tweet</Button>
+    </div>
+  )
+}
+
+export default Sidebar
