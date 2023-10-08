346223788049408c1ba395093b833ea5b44871af
Murp13 <suryarifin55@gmail.com>
2023-10-08 13:05:10 +0700

    styling 1

--- src/components/login.js
+++ src/components/login.js
@@ -3,6 +3,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 
 const validationSchema = Yup.object({
   usernameOrEmail: Yup.string().required("Username or Email is required"),
@@ -13,40 +14,84 @@ function Login() {
   const navigate = useNavigate();
 
   const handleSubmit = (values, { resetForm }) => {
-    axios.get("http://localhost:3001/users", { params: { usernameOrEmail: values.usernameOrEmail, password: values.password } }).then((response) => {
-      if (response.data.length === 1) {
-        resetForm();
-        navigate("/timeline");
-      } else {
-        alert("Invalid username/email or password.");
-      }
-    });
+    axios
+      .get("http://localhost:3001/users", {
+        params: {
+          usernameOrEmail: values.usernameOrEmail,
+          password: values.password,
+        },
+      })
+      .then((response) => {
+        if (response.data.length === 1) {
+          resetForm();
+          navigate("/timeline");
+        } else {
+          alert("Invalid username/email or password.");
+        }
+      });
   };
 
   const handleToGoRegister = () => {
-    navigate('/register');
+    navigate("/register");
   };
 
   return (
+    <Flex
+      direction="column"
+      align="center"
+      justify="flex-start"
+      minHeight="100vh"
+    >
+      <Flex
+        width="30%"
+        p="4"
+        justify="space-between"
+        align="center"
+        borderBottom="1px solid #ccc"
+        mb="4"
+        bgColor={'Black'}
+        color={"White"}
+      >
+        <Text fontSize="xl">Yuk!</Text>
+        <Button colorScheme="teal" variant="outline" onClick={handleToGoRegister}>
+          Register
+        </Button>
+      </Flex>
+      <Box width="100%" maxW="463px" p="4" textAlign="center" bgColor={"Yellow"}>
+  <Text fontSize="2xl" fontWeight="bold" mb="4">
+    Login
+  </Text>
+  <Formik
+    initialValues={{ usernameOrEmail: "", password: "" }}
+    validationSchema={validationSchema}
+    onSubmit={handleSubmit}
+  >
+    <Form>
     <div>
-      <button onClick={handleToGoRegister}>register</button>
-      <h2>Login</h2>
-      <Formik initialValues={{ usernameOrEmail: "", password: "" }} validationSchema={validationSchema} onSubmit={handleSubmit}>
-        <Form>
-          <div>
-            <label>Username or Email:</label>
-            <Field type="text" name="usernameOrEmail" />
-            <ErrorMessage name="usernameOrEmail" component="div" className="error" />
-          </div>
-          <div>
-            <label>Password:</label>
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" className="error" />
-          </div>
-          <button type="submit">Login</button>
-        </Form>
-      </Formik>
-    </div>
+        <Text mb={1}>Username or Email:</Text>
+        <Field type="text" name="usernameOrEmail"  />
+        <ErrorMessage
+          name="usernameOrEmail"
+          component="div"
+          className="error"
+        />
+      </div>
+      <div>
+        <Text mb={1}>Password:</Text>
+        <Field type="password" name="password"  />
+        <ErrorMessage
+          name="password"
+          component="div"
+          className="error"
+        />
+      </div>
+      <Button type="submit" colorScheme="teal" mb = "9" mt="4" width="30%">
+        Login
+      </Button>
+    </Form>
+  </Formik>
+</Box>
+</Flex>
   );
 }
 
--- src/components/register.js
+++ src/components/register.js
@@ -4,6 +4,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Box, Button, Flex, Text, Input } from "@chakra-ui/react";
 
 const validationSchema = Yup.object({
   email: Yup.string().email("Invalid email address").required("Email is required"),
@@ -28,35 +29,72 @@ function Register() {
   };
 
   return (
-    <div>
-      <button onClick={handleToGoLogin}>Login</button>
-      <h2>Register</h2>
-      <Formik initialValues={{ email: "", username: "", password: "", confirmPassword: "" }} validationSchema={validationSchema} onSubmit={handleSubmit}>
+    <Flex
+    direction="column"
+    align="center"
+    justify="flex-start" // Center vertically
+    minHeight="100vh"
+  >
+    <Flex
+      width="30%"
+      p="4"
+      justify="space-between"
+      align="center"
+      borderBottom="1px solid #ccc"
+      mb="4"
+      bgColor={'black'}
+      color={"white"}
+    >
+      <Text fontSize="xl">Yuk!</Text>
+      <Button colorScheme="teal" variant="outline" onClick={handleToGoLogin}>
+        Login
+      </Button>
+    </Flex>
+    <Box width="100%" maxW="464px" p="4" textAlign="center" bgColor={"yellow"}>
+      <Text fontSize="2xl" fontWeight="bold" mb="4">
+        Register
+      </Text>
+      <Formik
+        initialValues={{ email: "", username: "", password: "", confirmPassword: "" }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
         <Form>
           <div>
-            <label>Email:</label>
-            <Field type="text" name="email" />
+            <Text fontSize="lg" mb="1">
+              Email:
+            </Text>
+            <Field type="text" name="email" as={Input} />
             <ErrorMessage name="email" component="div" className="error" />
           </div>
           <div>
-            <label>Username:</label>
-            <Field type="text" name="username" />
+            <Text fontSize="lg" mb="1">
+              Username:
+            </Text>
+            <Field type="text" name="username" as={Input} />
             <ErrorMessage name="username" component="div" className="error" />
           </div>
           <div>
-            <label>Password:</label>
-            <Field type="password" name="password" />
+            <Text fontSize="lg" mb="1">
+              Password:
+            </Text>
+            <Field type="password" name="password" as={Input} />
             <ErrorMessage name="password" component="div" className="error" />
           </div>
           <div>
-            <label>Confirm Password:</label>
-            <Field type="password" name="confirmPassword" />
+            <Text fontSize="lg" mb="1">
+              Confirm Password:
+            </Text>
+            <Field type="password" name="confirmPassword" as={Input} />
             <ErrorMessage name="confirmPassword" component="div" className="error" />
           </div>
-          <button type="submit">Register</button>
+          <Button type="submit" colorScheme="green" mb ="9" mt="5" width="30%">
+            Register
+          </Button>
         </Form>
       </Formik>
-    </div>
+    </Box>
+  </Flex>
   );
 }
 
