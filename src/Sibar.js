1235f02bd4efa43b82734d7c9c126cfe957df809
Yoel Wildanto <142624911+yoelwildanto@users.noreply.github.com>
2023-10-11 11:07:31 +0700

    Kerjaan yoel (#7)
    * sidebar
    
    * up
    
    * up

--- src/Sibar.js
+++ src/Sibar.js
@@ -27,7 +27,9 @@ const Sidebar = () => {
         {/* <SidebarOption  Icon={""} text="Profile"/> */}
         <SidebarOption  Icon={CgMoreO} text="More"/>
       
-        <Button mt={"40px"} w={"100%"} color={"white"} bg={"twitter.400"}>Tweet</Button>
+
+        <Button colorScheme="green" mt={"40px"} w={"100%"} color={"white"} bg={"twitter.400"}>Tweet</Button>
+
     </div>
   )
 }
